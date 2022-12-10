d546c41c7057fc335a6b833e5a4a40c49a6795ff
guniu <guniu555@o2.pl>
2022-12-10 18:39:16 +0100

    Create day10.py

---
+++ 2022/day10.py
@@ -0,0 +1,32 @@
+# https://adventofcode.com/2022/day/10
+
+List = open("E:\\Temp\\input10.txt")
+
+Cycles = range(20, 221, 40)
+cycle = 0
+sigSum = 0
+X = 1
+
+def drawPixel():
+    pixel = '#' if abs(X-(cycle-1)%40) <= 1 else '.'
+    print(pixel, end="")
+    if cycle % 40 == 0:
+        print()
+
+def tick():
+    global cycle, sigSum
+    cycle += 1
+    if cycle in Cycles: sigSum += X*cycle
+    drawPixel()
+
+for line in List:
+    line = line.split()
+    if line[0] == "addx":
+        for _ in range(2):
+            tick()
+        X += int(line[1])
+    elif line[0] == "noop":
+        tick()
+List.close()
+
+print(sigSum)
