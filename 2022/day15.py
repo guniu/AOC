8cb8f8981fe33cb6f37bc11081fc6d28f3dbbb24
guniu <guniu555@o2.pl>
2022-12-17 01:26:17 +0100

    Update day15.py

--- 2022/day15.py
+++ 2022/day15.py
@@ -16,7 +16,7 @@ def get_Y_ranges(Y):
         if dist_to_B >= dist_to_Y:
             r = dist_to_B - dist_to_Y
             Y_ranges.append([s[0]-r, s[0]+r])
-            if s[3] == Y:      exclude.add(s[2])
+            if s[3] == Y: exclude.add(s[2])
     return sorted(Y_ranges)
 
 def get_X_gap(Y_ranges, X=0):
@@ -30,6 +30,7 @@ part1_Y = 2000000
 Y_ranges = get_Y_ranges(part1_Y)
 minX = Y_ranges[0][0]
 maxX = max(Y_ranges, key=lambda k: k[1])[1]
+
 X = get_X_gap(Y_ranges, minX)
 pos = X-minX
 while X <= maxX:
@@ -37,7 +38,7 @@ while X <= maxX:
     X = get_X_gap(Y_ranges, minX)
     pos += X-minX
 
-print('part1:', pos-len(exclude))
+print('part 1:', pos-len(exclude))
 
 MAX = 4000000
 div = MAX//100
@@ -45,4 +46,4 @@ for Y in range(0, MAX+1):
     if Y % div == 0: print('Searching:', Y//div, '%')
     X = get_X_gap(get_Y_ranges(Y))
     if X <= MAX:
-        print('Found one at x:', X, 'y:', Y, ' part2:', X*MAX+Y)
+        print('Found one at x:', X, 'y:', Y, 'part 2:', X*MAX+Y)
