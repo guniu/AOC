481553db685c24434c88d3ca63e7d366181c24f9
guniu <guniu555@o2.pl>
2022-12-27 12:02:26 +0100

    Update day7.py

--- 2022/day7.py
+++ 2022/day7.py
@@ -2,32 +2,31 @@
 
 List = open("E:\\Temp\\input7.txt")
 
-currentDir = [[]]
+currentDir = []
 dirSizes = []
 
 for line in List:
     line = line.split()
     if line[:2] == ['$', 'cd']:
         if line[2] != '..':
-            currentDir[-1].append([0])
-            currentDir.append(currentDir[-1][-1])
+            currentDir.append(0)
         else:
-            dirSizes.append(currentDir.pop()[0])
-            currentDir[-1][0] += dirSizes[-1]
+            dirSizes.append(currentDir.pop())
+            currentDir[-1] += dirSizes[-1]
     elif line[0].isdecimal():
-        currentDir[-1][0] += int(line[0])
+        currentDir[-1] += int(line[0])
 
-if len(currentDir) > 2:
-    for i in range(len(currentDir)-2):
-        dirSizes.append(currentDir.pop()[0])
-        currentDir[-1][0] += dirSizes[-1]
+if len(currentDir) > 1:
+    for i in range(len(currentDir)-1):
+        dirSizes.append(currentDir.pop())
+        currentDir[-1] += dirSizes[-1]
 
-total = currentDir[-1][0]
+total = currentDir[-1]
 dirSizes.append(total)
 
 maxsize = 70000000
 free = 30000000
 needed = total + free - maxsize
 
-print(sum([s for s in dirSizes if s <= 100000]))
-print(min([s for s in dirSizes if s >= needed]) if needed > 0 else 0)
+print(sum(s for s in dirSizes if s <= 100000))
+print(min(s for s in dirSizes if s >= needed) if needed > 0 else 0)
