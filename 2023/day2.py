ee75270b79c4118951349f98cb171e82e09046c3
guniu <guniu555@o2.pl>
2023-12-02 07:38:19 +0100

    Create day2.py

---
+++ 2023/day2.py
@@ -0,0 +1,29 @@
+# ==================================================
+# https://adventofcode.com/2023/day/2
+
+file = open("E:\\Temp\\AOC2023\\day2.txt").read().splitlines()
+
+Cubes = {'red': 12, 'green': 13, 'blue': 14}
+
+IDsum = 0
+Psum = 0
+
+for line in file:
+    line = line.split(':')
+    gameID = int(line[0].split()[1])
+    c = line[1].replace(',', '').replace(';', '').split()
+    P = {}
+
+    for i in range(0, len(c), 2):
+        if Cubes[c[i+1]] < int(c[i]):
+            gameID = 0
+        if int(c[i]) > P.get(c[i+1], 0):
+            P[c[i+1]] = int(c[i])
+
+    IDsum += gameID
+    k = 1
+    for v in P.values():
+        k *= v
+    Psum += k
+
+print(IDsum, Psum)
