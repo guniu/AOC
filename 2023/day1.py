ea6f89df9265100682d0b517ecd741f585411a37
guniu <guniu555@o2.pl>
2023-12-03 08:13:10 +0100

    Update day1.py

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,7 +1,7 @@
 # ==================================================
 # https://adventofcode.com/2023/day/1
 
-file = open("E:\\Temp\\AOC2023\\day1.txt").read().splitlines()
+file = open("day1.txt").read().splitlines()
 
 numbers  = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
             'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
