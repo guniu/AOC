5ca2bf4eb1522fe23c54f030fac5f5df3cacbacf
guniu <guniu555@o2.pl>
2023-12-06 14:42:27 +0100

    Update day6.py

--- 2023/day6.py
+++ 2023/day6.py
@@ -8,19 +8,19 @@ file  = open("day6.txt").read().splitlines()
 Times = file[0].split()[1:]
 Dists = file[1].split()[1:]
 
-def get_wins(t, d):
-    t, d = int(t), int(d)
-    q  = sqrt(t**2 - 4*d)
-    x1 = int((t-q)/2)+1
-    x2 = ceil((t+q)/2)-1
-    return x2-x1+1
-
 # def get_wins(t, d):
 #     t, d = int(t), int(d)
 #     i = 1
 #     while i*(t-i) <= d:
 #         i += 1
 #     return t-i-i+1
+
+def get_wins(t, d):
+    t, d = int(t), int(d)
+    q  = sqrt(t**2 - 4*d)
+    x1 = int((t-q)/2)+1
+    x2 = ceil((t+q)/2)-1
+    return x2-x1+1
 
 wins = 1
 for t, d in zip(Times, Dists):
