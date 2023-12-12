684292f7c4c5323a1a4ed977a4ab96dd835664a1
guniu <guniu555@o2.pl>
2023-12-12 01:13:15 +0100

    Update day11.py

--- 2023/day11.py
+++ 2023/day11.py
@@ -3,43 +3,28 @@
 
 file = open("day11.txt").read().splitlines()
 
-def expand(file, M):
-    i = 0
+def expand(file, E):
     for r in range(len(file)):
-        r += i
-        if file[r].find('#') != -1:
-            continue
-        file.insert(r, '.'*len(file[r]))
-        M.append(r)
-        i += 1
+        if file[r].find('#') == -1:
+            E.append(r)
 
-Mr = []
-expand(file, Mr)
-file = list(map(''.join, zip(*file)))
-Mc = []
-expand(file, Mc)
-file = list(map(''.join, zip(*file)))
+Er = []
+expand(file, Er)
+Ec = []
+expand(list(map(''.join, zip(*file))), Ec)
 
 G = []
 for r in range(len(file)):
     c = file[r].find('#')
     while c != -1:
-        G.append([r, c])
+        er = sum(1 for e in Er if e < r)
+        ec = sum(1 for e in Ec if e < c)
+        G.append([r, c, er, ec])
         c = file[r].find('#', c+1)
 
-P = [abs(g2[0]-g1[0]) + abs(g2[1]-g1[1]) for i, g1 in enumerate(G) for g2 in G[i+1:]]
+P = [g2[0]+g2[2]-g1[0]-g1[2] + abs(g2[1]+g2[3]-g1[1]-g1[3]) for i, g1 in enumerate(G) for g2 in G[i+1:]]
 print(sum(P))
 
-n = 10**6 - 2
-P2 = []
-for i, g1 in enumerate(G):
-    for g2 in G[i+1:]:
-        m = 0
-        for r in Mr:
-            if g1[0] < r < g2[0] or g2[0] < r < g1[0]:
-                m += n
-        for c in Mc:
-            if g1[1] < c < g2[1] or g2[1] < c < g1[1]:
-                m += n
-        P2.append(abs(g2[0]-g1[0]) + abs(g2[1]-g1[1]) + m)
-print(sum(P2))
+n = 10**6 - 1
+P = [g2[0]+g2[2]*n-g1[0]-g1[2]*n + abs(g2[1]+g2[3]*n-g1[1]-g1[3]*n) for i, g1 in enumerate(G) for g2 in G[i+1:]]
+print(sum(P))
