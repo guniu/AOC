0a751343de8d5137b75d27e89c9b80dacb4d2776
guniu <guniu555@o2.pl>
2023-12-17 10:23:56 +0100

    Create day15.py

---
+++ 2023/day15.py
@@ -0,0 +1,29 @@
+# ==================================================
+# https://adventofcode.com/2023/day/15
+
+file = open("day15.txt").read().strip().split(',')
+
+def hush(step):
+    res = 0
+    for c in step:
+        res = ((res + ord(c)) * 17) % 256
+    return res
+
+p1 = 0
+Boxes = {}
+for step in file:
+    p1 += hush(step)
+
+    label, fl = step.replace('=', '-').split('-')
+    box = hush(label)
+
+    if Boxes.get(box) is None:
+        if fl: Boxes[box] = {label: int(fl)}
+    elif fl:
+        Boxes[box][label] = int(fl)
+    elif Boxes[box].get(label) is not None:
+        Boxes[box].pop(label)
+
+print(p1)
+
+print(sum((b+1)*i*f for b, l in Boxes.items() for i, f in enumerate(l.values(), 1)))
