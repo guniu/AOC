7db9a2de305cb78f688524d93094d46de557b8a3
guniu <guniu555@o2.pl>
2023-12-17 02:58:58 +0100

    Create day14.py

---
+++ 2023/day14.py
@@ -0,0 +1,37 @@
+# ==================================================
+# https://adventofcode.com/2023/day/14
+
+file = open("day14.txt").read().splitlines()
+
+States = {}
+
+def tilt(file):
+    for r in range(len(file)):
+        line = ''.join(file[r]).split('#')
+        for s in range(len(line)):
+            c = line[s].count('O')
+            line[s] = line[s].replace('O', '.').replace('.', 'O', c)
+        file[r] = '#'.join(line)
+
+def calc_load(file):
+    return sum(len(row)-c for row in file for c, e in enumerate(row) if e == 'O')
+
+def same_state(file, c):
+    h = hash(tuple(file))
+    if States.get(h) is None:
+        States[h] = c
+    elif (10**9 - c) % (c - States[h]) == 0:
+        return True
+    return False
+
+file = list(zip(*file))[::-1]
+tilt(file)
+print(calc_load(file))
+
+c = 0
+while not same_state(file, c):
+    for _ in range(4):
+        tilt(file)
+        file = list(zip(*file[::-1]))
+    c += 1
+print(calc_load(file))
