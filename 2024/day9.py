7db0864dc08990143844500561ce6ef5545154e5
guniu <guniu555@o2.pl>
2025-01-03 10:25:07 +0100

    Create day9.py

---
+++ 2024/day9.py
@@ -0,0 +1,59 @@
+# ==================================================
+# https://adventofcode.com/2024/day/9
+
+file = open("day9.txt").read().strip()
+disk = list(map(int, file))
+
+backID = (len(disk)-1)//2
+
+empty1 = disk[1::2]
+eID = 0
+p1 = disk[0]
+step = 1
+s1 = 0
+
+empty2 = disk[1::2]
+I = {f: 0 for f in range(1, 10)}
+S = [p2 := 0] + [p2 := p2+b for b in disk[:-1]]
+s2 = 0
+
+for f in disk[backID*2::-2]:
+    b = f
+    while b > 0:
+        while eID < backID and empty1[eID] == 0:
+            eID += 1
+            p1 += disk[eID*2]
+        if eID < backID:
+            k = min(empty1[eID], b)
+            empty1[eID] -= k
+            b -= k
+        else:
+            if step == 1:
+                if f == b:
+                    p1 -= disk[backID*2+1]-empty1[backID]+1
+                else:
+                    p1 -= f-b+1
+            else:
+                p1 -= disk[backID*2+1]
+            step = -1
+            k = -b
+            b = 0
+        for p1 in range(p1, p1+k, step):
+            s1 += p1 * backID
+        p1 += step
+
+    for i in range(I[f], backID):
+        if empty2[i] >= f:
+            I[f] = i
+            p2 = S[i*2+1] + disk[i*2+1] - empty2[i]
+            empty2[i] -= f
+            break
+    else:
+        p2 = S[backID*2]
+
+    for p2 in range(p2, p2+f):
+        s2 += p2 * backID
+
+    backID -= 1
+
+print(s1, s2)
